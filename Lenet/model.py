0dc9619f6b273101e5e74ed3e6d5b8c364f83ee3
mayinglinlin <847115494@qq.com>
2021-05-10 11:22:02 +0800

    Initial commit

---
+++ Lenet/model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class  Lenet(nn.model):
+    def __init__(self):
+        super(Lenet,self).__init__()
+
+
+
+    def forward(self,x):
+        print(" ")
---
+++ Lenet/predict.py
@@ -0,0 +1,4 @@
+
+
+
+print("hello,predict")
