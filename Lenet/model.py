54472c98fde8e1e85db1887da18c03153d099cf2
mayinglinlin <847115494@qq.com>
2021-05-12 11:30:59 +0800

    采用GPU训练

--- Lenet/model.py
+++ Lenet/model.py
@@ -8,22 +8,20 @@ import numpy as np
 class Lenet(nn.Module):
     def __init__(self):
         super(Lenet, self).__init__()
-        self.conv1 = nn.Conv2d(3,6,5)  # input32 output 28
+        self.conv1 = nn.Conv2d(3,16,5)  # input32 output 28
         self.pool1 = nn.MaxPool2d(2,2) # input 28 output 14
-        self.conv2 = nn.Conv2d(6,16,5) # input 14 output 10
+        self.conv2 = nn.Conv2d(16,32,5) # input 14 output 10
         self.pool2 = nn.MaxPool2d(2,2) # 5 * 5 * 16
-        self.fc1   = nn.Linear(5*5*16,120)
+        self.fc1   = nn.Linear(5*5*32,120)
         self.fc2   = nn.Linear(120,84)
         self.fc3   = nn.Linear(84,10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
+        x = F.relu(self.conv1(x))
         x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(x)
+        x = F.relu(self.conv2(x))
         x = self.pool2(x)
-        x  = x.view(-1,16*5*5)
+        x  = x.view(-1,32*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
--- Lenet/train.py
+++ Lenet/train.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
     train_sets = torchvision.datasets.CIFAR10(root="./data",train = True,
                                               download= False,transform=transform)
@@ -21,25 +22,30 @@ def main():
 
 
     net = Lenet()
+    net.to(device)
     loss_function = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(),lr = .001)
 
     for epoch in range(10):
         running_loss = 0.0
+        #enumerate(train_loader)  类型list  [[],[[tensor image],[tensor label]]]
+        # tensor image = [batch,channel,image.height,image.width]
+        # tensor label = [len(batch)]
         for step,data in enumerate(train_loader):
             inputs,labels = data
             optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = loss_function(outputs,labels)
+
+            outputs = net(inputs.to(device))
+            loss = loss_function(outputs,labels.to(device))
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
             if step % 500 == 499:
                 with torch.no_grad():
-                    outputs = net(val_images)
+                    outputs = net(val_images.to(device))
                     predict_y = torch.max(outputs,dim = 1)[1]
-                    accuracy =  torch.eq(predict_y,val_labels).sum().item()/val_labels.size(0)
+                    accuracy =  torch.eq(predict_y.to(device),val_labels.to(device)).sum().item()/val_labels.size(0)
                     print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
                           (epoch + 1, step + 1, running_loss / 500, accuracy))
                     running_loss = 0.0
